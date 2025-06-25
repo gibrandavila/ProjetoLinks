c042010ad1c39d5fe15d056e1659e3ed457cf546
gibrandavila <gibran.davila.pesquisador@gmail.com>
2025-06-25 20:56:24 -0300

    restaurando

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-
 function toggleMode() {
   const html = document.documentElement;
-  html.classList.toggle("light");
-
   const image = document.querySelector("#profile img");
   if (html.classList.contains("light")) {
-    image.setAttribute("src", "./assets/avatar2.png");
-  } else {
     image.setAttribute("src", "./assets/Avatar.png");
+  } else {
+    image.setAttribute("src", "./assets/avatar2.png");
   }
+  html.classList.toggle("light");
 }
